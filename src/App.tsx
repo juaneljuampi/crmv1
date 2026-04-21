70d0bb476d9e1c5140bc496557c56771fb49b862
Juaneljuampi <ju.cortesn@duocuc.cl>
2026-04-21 14:34:33 -0400

    mejora chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />     {/* 🔥 principal */}
+        <Route path="/chat" element={<Home />} /> {/* tu CRM/chat */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
