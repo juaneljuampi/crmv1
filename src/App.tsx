b6e8c8e0ddc933aead58acf26194a9676fa54663
Juaneljuampi <ju.cortesn@duocuc.cl>
2026-04-22 11:30:22 -0400

    formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Formulario1 from "./pages/Formulario1";
 
 function App() {
-  return (
-    <BrowserRouter basename="/tuproyecto">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/formulario1" element={<Formulario1 />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Home />;
 }
 
 export default App;
