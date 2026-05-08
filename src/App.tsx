29fc953b18614e5e207d4eaf92fe83d429c335de
Juaneljuampi <ju.cortesn@duocuc.cl>
2026-05-07 21:21:46 -0400

    routeos

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,30 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MainLayout from "./layouts/MainLayout";
 import Dashboard from "./pages/Dashboard";
-import Formulario1 from "./pages/Formulario1";
 import Chats from "./pages/Chats";
 import Clientes from "./pages/Clientes";
+import Categorias from "./pages/Categorias";
+import Mensajes from "./pages/Mensajes";
+import Formulario1 from "./pages/Formulario1";
 
 export default function App() {
   return (
     <BrowserRouter basename="/crmv1">
-      <MainLayout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/formulario1" element={<Formulario1 />} />
-          <Route path="/chats" element={<Chats />} />
-          <Route path="/clientes" element={<Clientes />} />
+      <Routes>
 
-        </Routes>
-      </MainLayout>
+        {/* 🔥 RUTAS CON DASHBOARD */}
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="chats" element={<Chats />} />
+          <Route path="clientes" element={<Clientes />} />
+          <Route path="categorias" element={<Categorias />} />
+          <Route path="mensajes" element={<Mensajes />} />
+        </Route>
+
+        {/* 🔥 FORMULARIO FUERA DEL DASHBOARD */}
+        <Route path="/formulario1" element={<Formulario1 />} />
+
+      </Routes>
     </BrowserRouter>
   );
 }
