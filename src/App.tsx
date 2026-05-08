4c41465bf882071d55200208b8ac6ed9866ca874
Juaneljuampi <ju.cortesn@duocuc.cl>
2026-05-07 20:07:46 -0400

    integracion de rutas

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,28 @@ import Categorias from "./pages/Categorias";
 import Mensajes from "./pages/Mensajes";
 import Dashboard from "./pages/Dashboard";
 import Chats from "./pages/Chats";
+import Formulario1 from "./pages/Formulario1";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   const [isAuth, setIsAuth] = useState(false);
-  const [page, setPage] = useState("dashboard");
 
   if (!isAuth) {
     return <Login onLogin={() => setIsAuth(true)} />;
   }
 
   return (
-    <MainLayout setPage={setPage}>
-      {page === "dashboard" && <Dashboard />}
-      {page === "chats" && <Chats/>}
-      {page === "clientes" && <Clientes />}
-      {page === "categorias" && <Categorias />}
-      {page === "mensajes" && <Mensajes />}
-    </MainLayout>
+    <HashRouter>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/chats" element={<Chats />} />
+          <Route path="/clientes" element={<Clientes />} />
+          <Route path="/categorias" element={<Categorias />} />
+          <Route path="/mensajes" element={<Mensajes />} />
+          <Route path="/formulario1" element={<Formulario1 />} />
+        </Routes>
+      </MainLayout>
+    </HashRouter>
   );
 }
