20d4c21c89a341ad49196f5871253e8f6ddb1860
Juaneljuampi <ju.cortesn@duocuc.cl>
2026-05-07 20:36:23 -0400

    integracion de dashboar

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,49 @@
-import { useState } from "react";
-import Login from "./pages/Login";
-import Clientes from "./pages/Clientes";
+import { useState, useEffect } from "react";
 import MainLayout from "./layouts/MainLayout";
 import "./styles/dashboard.css";
-
-import Categorias from "./pages/Categorias";
-import Mensajes from "./pages/Mensajes";
-import Dashboard from "./pages/Dashboard";
 import Chats from "./pages/Chats";
+
+import Dashboard from "./pages/Dashboard";
 import Formulario1 from "./pages/Formulario1";
+import Clientes from "./pages/Clientes";
+
 export default function App() {
-  const [isAuth, setIsAuth] = useState(false);
   const [page, setPage] = useState("dashboard");
 
-  if (!isAuth) {
-    return <Login onLogin={() => setIsAuth(true)} />;
-  }
+  /**
+   * =========================
+   * 🔥 LEER URL AL CARGAR
+   * =========================
+   */
+  useEffect(() => {
+    const hash = window.location.hash.replace("#/", "");
 
+    if (hash === "formulario1") {
+      setPage("formulario1");
+    } else {
+      setPage("dashboard");
+    }
+  }, []);
+
+  /**
+   * =========================
+   * 🔥 ACTUALIZAR URL
+   * =========================
+   */
+  useEffect(() => {
+    window.location.hash = page;
+  }, [page]);
+
+  /**
+   * =========================
+   * RENDER
+   * =========================
+   */
   return (
     <MainLayout setPage={setPage}>
       {page === "dashboard" && <Dashboard />}
       {page === "chats" && <Chats/>}
       {page === "clientes" && <Clientes />}
-      {page === "categorias" && <Categorias />}
-      {page === "mensajes" && <Mensajes />}
       {page === "formulario1" && <Formulario1 />}
     </MainLayout>
   );
