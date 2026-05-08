9ffde23bb3849a7dd4d50bd44abd30fb20de7c7a
Juaneljuampi <ju.cortesn@duocuc.cl>
2026-05-07 20:46:02 -0400

    integracion de dashboar

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,18 @@
-import { useState, useEffect } from "react";
-import MainLayout from "./layouts/MainLayout";
-import "./styles/dashboard.css";
-import Chats from "./pages/Chats";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import MainLayout from "./layouts/MainLayout";
 import Dashboard from "./pages/Dashboard";
 import Formulario1 from "./pages/Formulario1";
-import Clientes from "./pages/Clientes";
 
 export default function App() {
-  const [page, setPage] = useState("dashboard");
-
-  /**
-   * =========================
-   * 🔥 LEER URL AL CARGAR
-   * =========================
-   */
-  useEffect(() => {
-    const hash = window.location.hash.replace("#/", "");
-
-    if (hash === "formulario1") {
-      setPage("formulario1");
-    } else {
-      setPage("dashboard");
-    }
-  }, []);
-
-  /**
-   * =========================
-   * 🔥 ACTUALIZAR URL
-   * =========================
-   */
-  useEffect(() => {
-    window.location.hash = page;
-  }, [page]);
-
-  /**
-   * =========================
-   * RENDER
-   * =========================
-   */
   return (
-    <MainLayout setPage={setPage}>
-      {page === "dashboard" && <Dashboard />}
-      {page === "chats" && <Chats/>}
-      {page === "clientes" && <Clientes />}
-      {page === "formulario1" && <Formulario1 />}
-    </MainLayout>
+    <BrowserRouter basename="/crmv1">
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/formulario1" element={<Formulario1 />} />
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
   );
 }
