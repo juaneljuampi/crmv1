918c765f32c90fc421a8721dfa204818b3e24726
Juaneljuampi <ju.cortesn@duocuc.cl>
2026-05-07 21:08:55 -0400

    integracion de dashboar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Dashboard from "./pages/Dashboard";
 import Formulario1 from "./pages/Formulario1";
+import Chats from "./pages/Chats";
+import Clientes from "./pages/Clientes";
 
 export default function App() {
   return (
@@ -11,6 +13,9 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/formulario1" element={<Formulario1 />} />
+          <Route path="/chats" element={<Chats />} />
+          <Route path="/clientes" element={<Clientes />} />
+
         </Routes>
       </MainLayout>
     </BrowserRouter>
